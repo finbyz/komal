5ec22ba93b9b01168f7cb0f16d6fdea43819737e
Finbyz <info@finbyz.tech>
2023-06-16 13:02:52 +0530

    ch

--- komal/api.py
+++ komal/api.py
@@ -3,6 +3,8 @@ import frappe
 from erpnext.controllers.accounts_controller import AccountsController
 
 def set_discount_in_quotation(self , method):
+	if frappe.session.user == "Administrator":
+		frappe.msgprint("KOMAL")
 	if self.discount:
 		for row in self.items:
 			row.discount_percentage = self.discount
