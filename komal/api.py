b68dbcc248fdc5007be2427fd186788554e65b21
Finbyz <info@finbyz.tech>
2023-06-16 13:17:01 +0530

    ch

--- komal/api.py
+++ komal/api.py
@@ -9,6 +9,7 @@ def set_discount_in_quotation(self , method):
 		for row in self.items:
 			row.discount_percentage = self.discount
 			row.discount_amount = (row.price_list_rate * self.discount)/100
+			frappe.db.set_value(row.doctype , row.name , "discount_amount" , row.discount_amount)
 			if(self.discount == 0):
 				row.discount_percentage = 0
 				row.discount_amount = 0	
@@ -16,5 +17,7 @@ def set_discount_in_quotation(self , method):
 		for row in self.items:
 			row.discount_percentage = 0
 			row.discount_amount = 0	
+			frappe.db.set_value(row.doctype , row.name , "discount_amount" , 0)
+	AccountsController.apply_pricing_rule_on_item(self)
 	AccountsController.calculate_taxes_and_totals(self)
 	AccountsController.set_missing_item_details(self)
